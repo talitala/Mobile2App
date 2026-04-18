ae863cb27882289712c8ed9710f815d3cea7d8fd
Tali <thaliastafford@gmail.com>
2026-04-18 10:17:22 -0400

    Initial commit

---
+++ app/src/main/java/com/mobile2/app/DataDisplayActivity.java
@@ -0,0 +1,12 @@
+package com.mobile2.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DataDisplayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_display);
+    }
+}
